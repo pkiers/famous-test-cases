20a195bb87e65e126579ce3263e139a871665761
Peter Kiers <pkiers@MacBook-Pro-van-Peter.local>
2014-08-11 11:37:39 -0700

    Initial ClickFix code

--- app/src/main.js
+++ app/src/main.js
@@ -1,31 +1,56 @@
 /* globals define */
 define(function(require, exports, module) {
     'use strict';
+
+    // ClickFix, remove to see clicks on swipe
+    require('famous/inputs/ClickFix');
+
     // import dependencies
     var Engine = require('famous/core/Engine');
-    var Modifier = require('famous/core/Modifier');
-    var Transform = require('famous/core/Transform');
-    var ImageSurface = require('famous/surfaces/ImageSurface');
+    var Surface = require('famous/core/Surface');
+    var Scrollview = require('famous/views/Scrollview');
+
+    var GenericSync = require("famous/inputs/GenericSync");
+    var MouseSync   = require("famous/inputs/MouseSync");
+    var TouchSync   = require("famous/inputs/TouchSync");
+
+    GenericSync.register({
+        mouse : MouseSync,
+        touch : TouchSync
+    });
 
     // create the main context
     var mainContext = Engine.createContext();
 
-    // your app here
-    mainContext.setPerspective(1000);
+    var scrollview = new Scrollview();
+    var surfaces = [];
+    scrollview.sequenceFrom(surfaces);
 
-    var logo = new ImageSurface({
-        size: [200, 200],
-        content: '/content/images/famous_logo.png',
-        classes: ['backfaceVisibility']
+    var sync = new GenericSync({
+        mouse : { direction : GenericSync.DIRECTION_Y },
+        touch : {direction : GenericSync.DIRECTION_Y }
     });
+    sync.pipe(scrollview);
 
-    var initialTime = Date.now();
-    var centerSpinModifier = new Modifier({
-        origin: [0.5, 0.5],
-        transform : function() {
-            return Transform.rotateY(.002 * (Date.now() - initialTime));
-        }
-    });
+    function _onClick() {
+        alert('clicked!');
+    }
 
-    mainContext.add(centerSpinModifier).add(logo);
+    for (var i = 0, temp; i < 40; i++) {
+        temp = new Surface({
+             content: "Surface: " + (i + 1),
+             size: [undefined, 200],
+             properties: {
+                 backgroundColor: "hsl(" + (i * 360 / 40) + ", 100%, 50%)",
+                 lineHeight: "200px",
+                 textAlign: "center"
+             }
+        });
+
+        temp.pipe(sync);
+        temp.on('click', _onClick)
+        surfaces.push(temp);
+    }
+
+    mainContext.add(scrollview);
 });
